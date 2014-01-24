a10c22f24356dd4ccc47337039fc98415db3836a
boris <boris@iis.sinica.edu.tw>
2014-01-24 19:51:12 +0800

    Documentation improvement

--- README.md
+++ README.md
@@ -5,12 +5,12 @@
 ## Usage
 
 The following usage example takes use of `y_support/name_magic` features.
-Please install [y_support gem] before trying it.
+Please install `y_support` gem before trying `yzz`.
 ```ruby
   require 'yzz'
   require 'y_support/name_magic'
 
-  # Let's establish a new class and imbue it with Yzz quality.
+  # Let's make a new class and imbue it with Yzz quality.
   #
   class Cell
     include Yzz, NameMagic
@@ -19,7 +19,7 @@ Please install [y_support gem] before trying it.
     end
   end
 
-  # Let's create 6 new cells.
+  # Let's now create 6 new ZZ cells.
   A1, A2, A3, B1, B2, B3 = 6.times.map { Cell.new }
 
   # And let's connect them along :x and :y dimensions:
@@ -29,7 +29,7 @@ Please install [y_support gem] before trying it.
   A2.along( :y ) >> B2
   A3.along( :y ) >> B3
 
-  # The Zz structure that we have created looks like this:
+  # The resulting ZZ structure looks like this:
   #
   #   a1 --> a2 --> a3
   #   |      |      |
@@ -37,21 +37,22 @@ Please install [y_support gem] before trying it.
   #   v      v      v
   #   b1 --> b2 --> b3
   #
-  # The above structure is a little bit like a small spreadsheet 3×2.
+  # It looks a bit like a small spreadsheet 3×2.
 
-  # The structure can be investigated:
-  A1.neighbors # gives all the neighbors of A1
+  # We can now explore this structure:
+  A1.neighbors # returns all the neighbors of A1
   #=> [A2, B1]
   A1.towards A2 # returns all the sides of A1 facing A2
+  # (note that A1 and A2 can be neighbors along more than one dimension)
   #=> [#<Yzz::Side: A1 along x, posward>]
-  A1.tw A2 # more concise way of showing the sides facing A2
+  A1.tw A2 # instead of "towards", we can just say "tw"
   #=> x->
   A1.tw B1
   #=> y->
 
-  # Of course, more complicated Zz structures can be created. See the [online
-  # presentation at xanadu.com for more](http://xanadu.com/zigzag/). For a small
-  # example here, let's create 2 diagonal connections:
+  # Of course, more complicated Zz structures can be created. See the online
+  # presentation at xanadu.com for more ( http://xanadu.com/zigzag/ ). For
+  # a small example here, let's create 2 diagonal connections:
 
   A1.along( :diagonal ) >> B2
   A2.along( :diagonal ) >> B3
@@ -60,6 +61,10 @@ Please install [y_support gem] before trying it.
   A1.neighbors
   #=> [A2, B1, B2]
 ```
+
+Again, the concern of `yzz` gem is to only provide the structure itself,
+rather than a comprehensive interface for interaction, visualization and
+other possible tasks. For these, you may check `y_nelson` gem.
 
 ## Contributing
 
--- lib/yzz.rb
+++ lib/yzz.rb
@@ -4,29 +4,28 @@ require_relative 'yzz/version'
 require_relative 'yzz/side'
 require_relative 'yzz/side_pair'
 
-# Yzz implements Ted Nelson's zz (hyperorthogonal, Zig-Zag...) structure. It
-# provides a mixin that imbues objects with zz properties.
+# Yzz implements Ted Nelson's ZZ (aka. Zig-Zag) structure. Yzz provides a mixin
+# that imbues objects with ZZ properties.
 # 
-# A zz structure consists of zz objects, which exist in multiple dimensions. Zz
-# objects can be connected by directed edges -- connections. Connected objects
-# are called neighbors. Each connection belongs to some dimension. A zz object
-# is considered as having two <em>sides</em> in each dimension: posward side and
-# negward side. A connection always points away from the posward side, and
-# towards the neighbor's negward side. In each dimension, zz object can have at
-# most one posward and one negward neighbor. A zz object can be connected to
-# itself, forming a loop.
+# A ZZ structure consists of ZZ objects, which exist in multiple dimensions. ZZ
+# objects may be connected by directed edges -- connections. Connected objects
+# are called neighbors. Each connection belongs to exactly one dimension. A ZZ
+# object is considered as having two <em>sides</em> in each dimension:
+# <em>posward</em> side and <em>negward</em> side. A connection always points
+# away from the posward side, and towards the negward side of the neighbor. In
+# each dimension, a ZZ object can have at most one posward and one negward
+# neighbor. The relation is bijective: If B is the posward neighbor of A along
+# dimension X, then A is a negward neighbor of B along X, and vice-versa. There
+# is no limitation as to what objects can be connected. Circles are allowed. A ZZ
+# object can even be connected to itself, forming a loop.
 # 
-# To these basic properties, Ted Nelson adds a bunch of other terminology.
-# A rank is a series of zz objects connected along one dimension. A rank viewed
-# horizontally is referred to as row. A rank viewed vertically is referred to
-# as column.
-# 
-# Mixin defining Zz structure (aka. hyperorthogonal structure). As represented
-# by YTed::Zz, a Zz structure is a collection of objects, whose connectivity is
-# defined in a multidimensional space in such way, that each object, along each
-# dimension, has at most one posward neighbor, and one negward neighbor. The
-# relation is bijective: If B is a posward neighbor of A along some dimension,
-# then A must be a negward neighbor of B along that dimension, and vice-versa.
+# To this basic definition, Ted Nelson adds a bunch of additional terminology.
+# A <em>rank</em> is a series of ZZ objects connected along the same dimension.
+# A rank viewed horizontally is referred to as <em>row</em>. A rank viewed
+# vertically is referred to as <em>column</em>. Ted Nelson's terms related not
+# to the ZZ structure itself, but rather to the proposed user interface (such
+# as <em>cursor</em>, <em>view</em>...) are not implemented in yzz, but rather
+# in <em>y_nelson</em> gem.
 # 
 module Yzz
   # Adds initialization of the @zz_dimensions hash to #initialize.
